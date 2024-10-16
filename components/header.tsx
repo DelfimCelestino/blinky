360da27bd3cc20f73a343c4e47c568936f006273
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-10-16 15:27:18 +0200

    fix: changing header zindex

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import AddProjectButton from "@/components/add-project-button";
 
 const Header = () => {
   return (
-    <div className="container mx-auto h-16 fixed top-0 left-0 right-0 bg-background flex items-center justify-between px-2">
+    <div className="z-50 container mx-auto h-16 fixed top-0 left-0 right-0 bg-background flex items-center justify-between px-2">
       <div className="flex items-center gap-2">
         <div className="relative h-10 w-10 lg:h-16 lg:w-20">
           <Image
