762d4491f369f34ded087317e1da375feb216450
Delfim Celestino <delfimcelestinoamissepastola@gmail.com>
2024-10-16 15:50:08 +0200

    chore adding google adsense as metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import PWAInstallPrompt from "@/components/pwa-install";
 import Header from "@/components/header";
 import { UniversalProvider } from "@/context/Universal-context-provider";
-import Script from "next/script";
 
 const roboto_condensed = Roboto_Condensed({ subsets: ["latin"] });
 
@@ -20,6 +19,9 @@ export const metadata: Metadata = {
     shortcut: "/icon512_rounded.png",
     apple: "/icon512_maskable.png",
   },
+  other: {
+    "google-adsense-account": "ca-pub-4787090241107157",
+  },
 };
 
 export default function RootLayout({
@@ -29,14 +31,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4787090241107157"
-          crossOrigin="anonymous"
-          strategy="lazyOnload"
-        />
-      </head>
       <body className={roboto_condensed.className}>
         <ThemeProvider
           attribute="class"
